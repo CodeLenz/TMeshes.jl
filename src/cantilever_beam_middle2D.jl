f53a60cfffa67a88fbd0f9f23f052499cbfbbdc6
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-17 14:42:39 -0400

    Update cantilever_beam_middle2D.jl

--- src/cantilever_beam_middle2D.jl
+++ src/cantilever_beam_middle2D.jl
@@ -10,7 +10,7 @@
 #
 
 function Cantilever_beam_middle2D(nx::Int64,ny::Int64,etype=:truss2D;   
-                                  Lx=1.0, Ly=1.0, force=1.0, A=1E-4, Ex=1E9,
+                                  Lx=8.0, Ly=5.0, force=1.0, A=1E-4, Ex=1E9,
                                   thickness=0.1)
 
 
