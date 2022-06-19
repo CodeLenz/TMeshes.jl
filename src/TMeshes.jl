e2008c821ad37434072f8552bf627c15f187f499
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-19 13:29:50 -0400

    Simply_supported3D

--- src/TMeshes.jl
+++ src/TMeshes.jl
@@ -13,9 +13,12 @@ module TMeshes
 using BMesh, LMesh
 
 include("simply_supported2D.jl")
+include("simply_supported3D.jl")
 include("cantilever_beam_bottom2D.jl")
 include("cantilever_beam_middle2D.jl")
 
-export Simply_supported2D,Cantilever_beam_bottom2D, Cantilever_beam_middle2D
+export Simply_supported2D, Simply_supported3D
+export Cantilever_beam_bottom2D
+export Cantilever_beam_middle2D
 
 end # module
