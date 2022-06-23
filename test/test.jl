847727c000340f3c339df43c9f1b564fab56811a
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 11:32:57 -0300

    Fixing typo
    @test_throws AssertionError

--- test/test.jl
+++ test/test.jl
@@ -9,7 +9,7 @@
 
     # Should throw
     # @assert etype==:truss2D || etype==:solid2D "Cantilever_beam_bottom2D::etype must be truss2D or solid2D"
-    @test_throw AssertionError Cantilever_beam_bottom2D(2,2,:bla)
+    @test_throws AssertionError Cantilever_beam_bottom2D(2,2,:bla)
 
 
     #################### Cantilever_beam_middle2D ##########################
@@ -21,10 +21,10 @@
 
     # Should throw
     # @assert etype==:truss2D || etype==:solid2D "Cantilever_beam_middle2D::etype must be truss2D or solid2D"
-    @test_throw AssertionError Cantilever_beam_middle2D(2,2,:bla)
+    @test_throws AssertionError Cantilever_beam_middle2D(2,2,:bla)
 
     #@assert iseven(ny) "Cantilever_Beam_Middle2D::ny must be even"
-    @test_throw AssertionError Cantilever_beam_middle2D(2,3)
+    @test_throws AssertionError Cantilever_beam_middle2D(2,3)
 
 
     #################### Simply_supported2D ##########################
@@ -36,10 +36,10 @@
 
     # Should throw
     # @assert etype==:truss2D || etype==:solid2D "Simply_supported2D::etype must be truss2D or solid2D"
-    @test_throw AssertionError Simply_supported2D(2,2,:bla)
+    @test_throws AssertionError Simply_supported2D(2,2,:bla)
 
     #@assert iseven(ny) "Simply_supported2D::nx must be even"
-    @test_throw AssertionError Simply_supported2D(3,2)
+    @test_throws AssertionError Simply_supported2D(3,2)
 
 
     #################### Simply_supported3D ##########################
@@ -49,12 +49,12 @@
 
     # Should throw
     # @assert iseven(nx)&&iseven(ny) "Simply_Supported3D::nx and ny must be even"
-    @test_throw AssertionError Simply_supported2D(3,2,2)
-    @test_throw AssertionError Simply_supported2D(2,3,2)
-    @test_throw AssertionError Simply_supported2D(3,3,2)
+    @test_throws AssertionError Simply_supported2D(3,2,2)
+    @test_throws AssertionError Simply_supported2D(2,3,2)
+    @test_throws AssertionError Simply_supported2D(3,3,2)
        
     # @assert etype==:truss3D "Simply_Supported3D::etype must be truss3D"
-    @test_throw AssertionError Simply_supported2D(2,2,2,:bla)
-    @test_throw AssertionError Simply_supported2D(2,2,2,:solid3D)
+    @test_throws AssertionError Simply_supported2D(2,2,2,:bla)
+    @test_throws AssertionError Simply_supported2D(2,2,2,:solid3D)
 
 end
