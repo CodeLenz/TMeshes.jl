5ef15f6f4d2770c332e292c489832856a3d075fd
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 11:06:08 -0300

    Adding density

--- src/simply_supported2D.jl
+++ src/simply_supported2D.jl
@@ -10,7 +10,7 @@
 #
 function Simply_supported2D(nx::Int64,ny::Int64,etype=:truss2D;   
                           Lx=1.0, Ly=1.0, force=1.0, A=1E-4 ,Ex=1E9,
-                          thickness=0.1)
+                          density=7850.0,thickness=0.1)
 
 
 
@@ -37,7 +37,7 @@ function Simply_supported2D(nx::Int64,ny::Int64,etype=:truss2D;
     nbc = [no_forca 2 -force]
 
     # Vamos definir Ex e A "fixos" -> valores muito "chutados"
-    mat = [Material(Ex=Ex)]
+    mat = [Material(Ex=Ex,density=density)]
     geom = [Geometry(A=A, thickness=thickness)]
 
     # Gera a malha e devolve um tipo Mesh2D
